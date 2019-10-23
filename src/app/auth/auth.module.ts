eb23da571e432d7dc632909775e7724fd3598e9a
Jingting <jchen136@hawk.iit.edu>
2019-10-23 11:27:07 -0500

    Create login and signup component

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { FormsModule } from '@angular/forms';
+import { AuthRoutingModule } from './auth-routing.module';
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    SignupComponent,
+  ],
+  imports: [
+   CommonModule,
+   FormsModule,
+   AuthRoutingModule
+  ]
+})
+export class AuthModule { }
