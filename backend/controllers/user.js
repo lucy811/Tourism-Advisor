042b8855e9ba48549d4d312e979153f46ef94538
Jingting <jchen136@hawk.iit.edu>
2019-10-25 10:34:19 -0500

    Add user login function

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,6 @@
   
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -25,3 +26,36 @@ exports.createUser = (req, res, next) => {
        });
    });
 }
+
+exports.userLogin = (req, res, next) => {
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        fetchedUser = user;
+        return bcrypt.compare(req.body.password, user.password);
+      }
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: 'Auth failed'
+        });
+      }
+      const token =jwt.sign(
+        {email: fetchedUser.email, userId: fetchedUser._id}, 
+        'secret_this_should_be_longer',
+        { expiresIn: '1h' } // token will expire after 1 hour
+      );
+      res.status(200).json({
+        token: token,
+        expiresIn: 3600,
+        userId: fetchedUser._id
+      });
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: 'Invalid authentication credentials!'
+      });
+    });
+ }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,5 +5,6 @@ const UserController = require('../controllers/user');
 const router = express.Router();
 
 router.post('/api/user/signup', UserController.createUser);
+router.post('/api/user/login', UserController.userLogin);
 
 module.exports = router;
