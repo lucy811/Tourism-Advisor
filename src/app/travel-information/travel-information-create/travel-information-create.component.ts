76d7bbb66a89050e4d4871d35eda9788125762f3
Jingting <jchen136@hawk.iit.edu>
2019-10-28 15:44:53 -0500

    Add image preview

--- src/app/travel-information/travel-information-create/travel-information-create.component.ts
+++ src/app/travel-information/travel-information-create/travel-information-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { mimeType } from './mime-type.validator';
 
 @Component({
   selector: 'app-travel-information-create',
@@ -8,6 +9,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class TravelInformationCreateComponent implements OnInit {
   private mode = 'create';
+  imagePreview: any;
   travelInfoForm: FormGroup;
 
   constructor() { }
@@ -16,12 +18,28 @@ export class TravelInformationCreateComponent implements OnInit {
     this.travelInfoForm = new FormGroup({
       name: new FormControl(null, {validators: [Validators.required]}),
       price: new FormControl(null, {validators: [Validators.required]}),
-      image: new FormControl(null, {validators: [Validators.required]}),
-      description: new FormControl(null, {validators: [Validators.required]})
+      description: new FormControl(null, {validators: [Validators.required]}),
+      image: new FormControl(null, {validators: [Validators.required], asyncValidators: [mimeType]})
     });
   }
 
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.travelInfoForm.patchValue({image: file});
+    // informs Angular that I changed the value and it should re-evaluate that, store the value
+    this.travelInfoForm.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+      console.log('imagePreview: ' + this.imagePreview);
+    };
+    reader.readAsDataURL(file);
+  }
+
   onSaveTravalInfo() {
-    
+    if (this.travelInfoForm.invalid) {
+      return ;
+    }
+    console.log('form value: ' + JSON.stringify(this.travelInfoForm.value));
   }
 }
