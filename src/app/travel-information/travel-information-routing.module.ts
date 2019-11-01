f0d229eae626a13aa5a5830d4026ba51ce830554
Jingting <jchen136@hawk.iit.edu>
2019-11-01 11:28:55 -0500

    Create travel-information-detail component

--- src/app/travel-information/travel-information-routing.module.ts
+++ src/app/travel-information/travel-information-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TravelInformationCreateComponent } from './travel-information-create/travel-information-create.component';
 import { TravelInformationCollectionComponent } from './travel-information-collection/travel-information-collection.component';
+import { TravelInformationDetailComponent } from './travel-information-detail/travel-information-detail.component';
 
 const travelRoutes: Routes = [
   { path: 'travel-info-collection', component: TravelInformationCollectionComponent },
   { path: 'travel-info-create', component: TravelInformationCreateComponent },
+  { path: 'travel-info-detail', component: TravelInformationDetailComponent}
 ];
 
 @NgModule({
--- src/app/travel-information/travel-information.module.ts
+++ src/app/travel-information/travel-information.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { TravelInformationCollectionComponent } from './travel-information-collection/travel-information-collection.component';
 import { TravelInformationCreateComponent } from './travel-information-create/travel-information-create.component';
+import { TravelInformationDetailComponent } from './travel-information-detail/travel-information-detail.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { TravelRoutingModule } from './travel-infromation-routing.module';
+import { TravelRoutingModule } from './travel-information-routing.module';
 
 @NgModule({
   declarations: [
     TravelInformationCollectionComponent,
     TravelInformationCreateComponent,
+    TravelInformationDetailComponent
   ],
   imports: [
     CommonModule,
