124432861c4b5b7c92212700b93fd573be244619
Jingting <jchen136@hawk.iit.edu>
2019-11-01 11:46:35 -0500

    Add logout function in frontend

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -24,6 +24,10 @@ export class HeaderComponent implements OnInit {
     });
   }
 
+  logout() {
+    this.authService.logout();
+  }
+
   ngOnDestroy() {
     this.authListenerSubs.unsubscribe();
   }
