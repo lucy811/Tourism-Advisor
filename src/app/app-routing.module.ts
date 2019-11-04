ed60a7301f317ce835a0044fdb32d1a8f78184bb
Jingting <jchen136@hawk.iit.edu>
2019-11-04 15:04:10 -0600

    Add AuthGuard to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './core/home/home.component';
-import { TravelInformationCollectionComponent } from './travel-information/travel-information-collection/travel-information-collection.component';
-import { TravelInformationCreateComponent } from './travel-information/travel-information-create/travel-information-create.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'travel-info-collection', component: TravelInformationCollectionComponent },
-  { path: 'travel-info-create', component: TravelInformationCreateComponent }
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
