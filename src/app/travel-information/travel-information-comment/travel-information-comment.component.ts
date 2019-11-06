a192eb4d20861bd19e8a5afdcfb951afea6fcb30
Jingting <jchen136@hawk.iit.edu>
2019-11-05 22:24:28 -0600

    Add travel-information-comment component

---
+++ src/app/travel-information/travel-information-comment/travel-information-comment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-travel-information-comment',
+  templateUrl: './travel-information-comment.component.html',
+  styleUrls: ['./travel-information-comment.component.scss']
+})
+export class TravelInformationCommentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
