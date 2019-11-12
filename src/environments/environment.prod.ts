603ddbe531acfde20f0dc36cf819846787380bb6
Jingting <jchen136@hawk.iit.edu>
2019-11-12 12:04:48 -0600

    Update environments file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '0.0.0.0:3000/api'
+  apiUrl: 'localhost:3000/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: '0.0.0.0:3000/api'
+  apiUrl: 'localhost:3000/api'
 };
 
 /*
